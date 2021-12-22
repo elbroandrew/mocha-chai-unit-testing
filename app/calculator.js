e37692cee805c7cd4313cd7e3efeea7b546d5dae
Andrew <elbroandrew@gmail.com>
2021-12-22 15:02:44 +1000

    feat: add calculator class, add and multiply methods to test

--- app/calculator.js
+++ app/calculator.js
@@ -1,8 +1,37 @@
 /**
  *
- *
+ * a class for calculator
  * @class Calculator
  */
-class Calculator{
+class Calculator {
+  /**
+   * Creates an instance of Calculator.
+   * @memberof Calculator
+   */
+  constructor() {
 
-}
+  };
+
+  /**
+   *
+   *
+   * @param {Number} a a float or integer number
+   * @param {Number} b a float or integer number
+   * @return {Number} foat or integer result
+   * @memberof Calculator
+   */
+  add(a, b) {
+    return a + b;
+  };
+  /**
+   *
+   *
+   * @param {Number} a a float or integer
+   * @param {Number} b a float or integer
+   * @return {Number} float or integer result
+   * @memberof Calculator
+   */
+  multiply(a, b) {
+    return a * b;
+  }
+};
