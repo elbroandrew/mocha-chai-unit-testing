a69e407991d6af6e4167b473e713e24c84733280
Andrew <elbroandrew@gmail.com>
2021-12-22 14:11:32 +1000

    feat: add chai tests

--- test/specs/isNumberEven.spec.js
+++ test/specs/isNumberEven.spec.js
@@ -12,4 +12,16 @@ describe(`isNumberEven positive scenarios`, () => {
   it(`should return true if number is even`, () => {
     expect(validator.isNumberEven(4)).to.be.equal(true);
   });
+  it(`should return false if number is odd`, () => {
+    expect(validator.isNumberEven(5)).to.be.equal(false);
+  });
+  it(`chai test`, () => {
+    expect({a: 'apple', b: 30}).to.be.have.property('a');
+  });
+  it(`chai test`, () => {
+    expect({a: 'apple', b: 30}).to.be.eql({a: 'apple', b: 30});
+  });
+  it(`chai test`, () => {
+    expect([1,2,3]).to.have.lengthOf(3);
+  });
 });
